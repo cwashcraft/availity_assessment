d414eab60b4a5edbe8a70a873b894088c7cee9d7
Chris Ashcraft <chris.ashcraft@hhchealth.org>
2025-01-22 18:20:57 -0500

    solution

--- src/main/scala/com/availity/spark/provider/ProviderRoster.scala
+++ src/main/scala/com/availity/spark/provider/ProviderRoster.scala
@@ -1,12 +1,113 @@
 package com.availity.spark.provider
 
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.types.{StructType, DateType, StringType}
+import org.apache.spark.sql.functions.{coalesce, col, concat, count, date_format, lit, month, sum, when}
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
-import org.apache.spark.sql.functions.{count, lit, array, collect_list, col, month, avg}
+/**
+ * Performs aggregations over provider and visit data
+ * @param ss spark session object
+ * @param providerPath path to the provider file
+ * @param visitPath path to the visits file
+ */
+class ProviderRoster(ss: SparkSession, providerPath: String, visitPath: String)  {
 
-object ProviderRoster  {
+  /**
+   * Reads CSV data into a dataframe suitable for downstream operations
+   * @return DataFrame of joined provider and visit data
+   */
+  def readData(): DataFrame = {
 
-  def main(args: Array[String]): Unit = {}
+    // read in the providers.csv
+    def providers: DataFrame = ss.read.format("csv")
+      .options(Map("header" -> "true", "delimiter" -> "|"))
+      .load(providerPath)
+      .withColumn("provider_id_int", coalesce(col("provider_id").cast("integer"), lit(0)))
+      .drop("provider_id")
+      .withColumnRenamed("provider_id_int", "provider_id")
 
+
+    // read in the visits.csv
+    def visits: DataFrame = ss.read.format("csv")
+      .options(Map("header" -> "false"))
+      .load(visitPath)
+      .withColumn("visit_id", coalesce(col("_c0").cast("integer"), lit(0)))
+      .withColumn("provider_id", coalesce(col("_c1").cast("integer"), lit(0)))
+      .withColumn("date_of_service", date_format(col("_c2"), "yyyy-MM-dd"))
+      .drop("_c0", "_c1", "_c2")
+
+    // join providers with their visits
+    providers.join(visits, Seq("provider_id"), "left_outer")
+  }
+
+  /**
+   * Given the two data datasets, calculate the total number of visits per provider.
+   * The resulting set should contain the provider's ID, name, specialty, along with
+   * the number of visits.
+   *
+   * @param df Input dataframe
+   * @return Aggregated data
+   */
+  def getProviderVisitCount(df: DataFrame): DataFrame = {
+    df.withColumn("provider_name", concat(col("last_name"), lit(", "), col("first_name"), lit(" " ), col("middle_name")))
+      .groupBy("provider_id", "provider_name", "provider_specialty")
+      .agg(count(col("visit_id")).alias("number_of_visits"))
+      .orderBy("provider_specialty", "provider_id", "provider_name")
+  }
+
+  /**
+   * Given the two datasets, calculate the total number of visits per provider per month.
+   * The resulting set should contain the provider's ID, the month, and total number of visits.
+   *
+   * @param df Input dataframe
+   * @return Aggregated data
+   */
+  def getProviderVisitCountPerMonth(df: DataFrame): DataFrame = {
+    df
+      .withColumn("month_of_service", date_format(col("date_of_service"), "MMM"))
+      .filter(col("visit_id").isNotNull)
+      .groupBy("provider_id", "month_of_service")
+      .agg(count(lit(1)).alias("number_of_visits"))
+      .orderBy("provider_id", "month_of_service")
+  }
+}
+
+/**
+ * Application entrypoint
+ */
+object Main extends App {
+
+  private val dataPath = "C:\\Users\\cashcraft\\IdeaProjects\\provider-roster\\data\\"
+  private val providerPath = dataPath + "providers.csv"
+  private val visitPath = dataPath + "visits.csv"
+
+  private val ss = new SparkSession.Builder().config("spark.master", "local").appName("my_app").getOrCreate()
+
+  // spark session and file paths are parameterizable to simplify unit testing
+  private val rObj = new ProviderRoster(ss, providerPath, visitPath)
+
+  // we'll reuse this dataframe for both operations to reduce IO and joins
+  private val providerVisits = rObj.readData()
+
+  // perform the aggregations
+  private val visitsPerProvider = rObj.getProviderVisitCount(providerVisits)
+  private val visitsPerProviderPerMonth = rObj.getProviderVisitCountPerMonth(providerVisits)
+
+  // output the visits per provider in json, partitioned by the provider's specialty.
+  visitsPerProvider
+    .repartition(1)  // we only want a single file per partition
+    .write
+    .format("json")
+    .mode("overwrite")
+    .partitionBy("provider_specialty")
+    .save("output/visitsPerProvider")
+
+  // output the visits per provider per month in json
+  visitsPerProviderPerMonth
+    .repartition(1) // we only want a single file output
+    .write
+    .format("json")
+    .mode("overwrite")
+    .save("output/visitsPerProviderPerMonth")
+
+  ss.stop()
 }
--- src/test/scala/com/availity/spark/provider/ProviderRosterSpec.scala
+++ src/test/scala/com/availity/spark/provider/ProviderRosterSpec.scala
@@ -1,20 +1,129 @@
 package com.availity.spark.provider
 
 import com.github.mrpowers.spark.fast.tests.DataFrameComparer
+import org.apache.spark.sql.types.{IntegerType, LongType, StringType, StructField, StructType}
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 import org.scalatest.BeforeAndAfterEach
 import org.scalatest.funspec.AnyFunSpec
 
+import java.nio.file.Paths
+import java.util.Objects
+
 class ProviderRosterSpec extends AnyFunSpec with DataFrameComparer with BeforeAndAfterEach {
 
-  override def beforeEach: Unit = {
+  @transient var ss: SparkSession = null
+
+  // read test data from resource location
+  val providersPath: String = Paths.get(Objects.requireNonNull(this.getClass.getClassLoader.getResource("test-providers.csv")).toURI).toAbsolutePath.toString
+  val visitsPath: String = Paths.get(Objects.requireNonNull(this.getClass.getClassLoader.getResource("test-visits.csv")).toURI).toAbsolutePath.toString
+
+  override def beforeEach(): Unit = {
+    ss = new SparkSession.Builder()
+      .config("spark.master", "local")
+      .appName("test_app")
+      .getOrCreate()
   }
 
   override def afterEach(): Unit = {
+    ss.stop()
   }
 
   describe("process") {
-    it("should run a test") {
-      ProviderRoster.process()
+
+    it ("visitCountsPerProvider") {
+
+      // define our expected output
+      val testData = Seq(
+        Row(2, "Block, Candice C", "Cardiology", 9L),
+        Row(7, "Barrows, Napoleon A", "Cardiology", 4L),
+        Row(8, "Reichel, Roy C", "Chiropractic", 1L),
+        Row(9, "Stokes, Lavon C", "Dermatology", 10L),
+        Row(4, "Brown, Lenny B", "Gastroenterology", 6L),
+        Row(10, "Leannon, Tiara A", "General Practice", 6L),
+        Row(3, "Bartell, Margaret B", "Psychiatry", 3L),
+        Row(5, "Daugherty, Julien B", "Rheumatology", 0L),
+        Row(6, "Mertz, Rowena A", "Sports Medicine", 4L),
+        Row(1, "Kuphal, Bessie B", "Urology", 12L),
+      )
+
+      val testSchema = List(
+        StructField("provider_id", IntegerType, nullable = false),
+        StructField("provider_name", StringType, nullable = true),
+        StructField("provider_specialty", StringType, nullable = true),
+        StructField("number_of_visits", LongType, nullable = false)
+      )
+
+      val expectedDF = ss.createDataFrame(ss.sparkContext.parallelize(testData), StructType(testSchema))
+
+      val rObj = new ProviderRoster(ss, providersPath, visitsPath)
+      val providerVisits = rObj.readData()
+      val actualDF = rObj.getProviderVisitCount(providerVisits)
+
+      assertSmallDataFrameEquality(actualDF, expectedDF)
+
+    }
+
+    it ("visitCountsPerProviderPerMonth") {
+
+      // define our expected output
+      val testData = Seq(
+        Row(1, "Apr", 1L),
+        Row(1, "Aug", 1L),
+        Row(1, "Dec", 2L),
+        Row(1, "Feb", 1L),
+        Row(1, "Jul", 3L),
+        Row(1, "Jun", 1L),
+        Row(1, "Mar", 1L),
+        Row(1, "Oct", 2L),
+        Row(2, "Apr", 1L),
+        Row(2, "Dec", 1L),
+        Row(2, "Jan", 1L),
+        Row(2, "Jun", 1L),
+        Row(2, "May", 1L),
+        Row(2, "Nov", 2L),
+        Row(2, "Oct", 1L),
+        Row(2, "Sep", 1L),
+        Row(3, "Apr", 1L),
+        Row(3, "Jul", 1L),
+        Row(3, "Nov", 1L),
+        Row(4, "Jul", 3L),
+        Row(4, "Nov", 1L),
+        Row(4, "Oct", 2L),
+        Row(6, "Jan", 1L),
+        Row(6, "Jul", 1L),
+        Row(6, "Oct", 1L),
+        Row(6, "Sep", 1L),
+        Row(7, "Aug", 1L),
+        Row(7, "Jul", 1L),
+        Row(7, "May", 1L),
+        Row(7, "Oct", 1L),
+        Row(8, "Jul", 1L),
+        Row(9, "Apr", 1L),
+        Row(9, "Aug", 1L),
+        Row(9, "Jan", 2L),
+        Row(9, "Jul", 2L),
+        Row(9, "Jun", 2L),
+        Row(9, "Mar", 1L),
+        Row(9, "Oct", 1L),
+        Row(10, "Dec", 4L),
+        Row(10, "Jul", 1L),
+        Row(10, "Oct", 1L)
+      )
+
+      val testSchema = List(
+        StructField("provider_id", IntegerType, nullable = false),
+        StructField("month_of_service", StringType, nullable = true),
+        StructField("number_of_visits", LongType, nullable = false)
+      )
+
+      val expectedDF = ss.createDataFrame(ss.sparkContext.parallelize(testData), StructType(testSchema))
+
+      val rObj = new ProviderRoster(ss, providersPath, visitsPath)
+      val providerVisits = rObj.readData()
+      val actualDF = rObj.getProviderVisitCountPerMonth(providerVisits)
+
+      assertSmallDataFrameEquality(actualDF, expectedDF)
+
     }
   }
 }
